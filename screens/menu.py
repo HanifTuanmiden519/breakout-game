fa34716a4304fb10c5cce01ea1ad4fafc2518076
ArafiPsu <6710110685@psu.ac.th>
2025-03-04 20:39:30 +0700

    จัดให้ระดับความยากอยุ่ระหว่างปุ่ม previous และ next

--- screens/menu.py
+++ screens/menu.py
@@ -19,36 +19,39 @@ class MenuScreen(Screen):
 
         layout = BoxLayout(orientation='vertical', spacing=10, padding=50)
 
-        self.label = Label(text=f"{get_difficulty().capitalize()}", font_size=24)
-        layout.add_widget(self.label)
+        
         
         # ปุ่ม 'Start Game'
         start_btn = Button(text="Start Game", font_size=24, size_hint=(1, 0.2))
         start_btn.bind(on_press=self.start_game)
-
-        # เพิ่มปุ่มลงใน layout
         layout.add_widget(start_btn)
         
-        arrows_layout = BoxLayout(size_hint=(1, 0.2))
         
+        
+        # สร้าง BoxLayout สำหรับปุ่ม 'Previous' และ 'Next'
+        arrows_layout = BoxLayout(size_hint=(1, 0.2))
         # ใช้ปุ่ม 'Previous' แทนลูกศรซ้าย
         prev_btn = Button(text="Previous", size_hint=(0.3, 1),
                   background_color=(1, 1, 1, 0.3),  # กำหนดสีเป็นโปร่งใส
                   background_normal='')
         prev_btn.bind(on_press=self.change_difficulty_left)
 
+        # สร้าง Label สำหรับแสดงความยาก
+        self.label = Label(text=f"{get_difficulty().capitalize()}", font_size=24)
+        
         # ใช้ปุ่ม 'Next' แทนลูกศรขวา
         next_btn = Button(text="Next", size_hint=(0.3, 1),
                   background_color=(1, 1, 1, 0.3),  # กำหนดสีเป็นโปร่งใส
                   background_normal='')
         next_btn.bind(on_press=self.change_difficulty_right)
 
+        # เพิ่ม widget ลงใน layout แนวนอน
         arrows_layout.add_widget(prev_btn)
+        arrows_layout.add_widget(self.label)  # ให้ Label อยู่ตรงกลาง
         arrows_layout.add_widget(next_btn)
 
         layout.add_widget(arrows_layout)
 
-        
         self.add_widget(layout)
 
     def update_background(self, *args):
